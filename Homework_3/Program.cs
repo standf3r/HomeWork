d527adf9f3b6daf0c40b7f13c7ce3ea66232e549
standfer <vertigl@yandex.ru>
2022-04-17 16:27:29 +0800

    Задача 47. Семинар 7

---
+++ Homework_3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 47. Массив MxN заполненный случайными вещественными числами
+
+int m = 3;
+int n = 4;
+
+double[,] array = new double[m,n];
+void FillArray(double[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().NextDouble();
+        }
+    }
+}
+
+void Print(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]}");
+        }
+    }
+}
+
+FillArray(array);
+Print(array);
