72bb9f2b692922d221c877fe7d7cd42d468301f8
standfer <vertigl@yandex.ru>
2022-04-17 17:42:36 +0800

    Задача 62. Семинар 9

---
+++ Homework_7/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 62. Заполнение массива спиралью
+
+Console.Write("Введите размерность: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[n, n];
+int temp = 1;
+int i = 0;
+int j = 0;
+while (temp <= n * n)
+{
+    array[i,j] = temp;
+    if (i <= j + 1 && i + j < n - 1)
+    ++j;
+    else if (i< j && i + j >= n - 1)
+    ++i;
+    else if(i >= j && i + j > n - 1)
+    --j;
+    else
+    --i;
+    ++temp;
+}
+
+for(int x = 0; x < array.GetLength(0); x++)
+{
+    for (int s = 0; s < array.GetLength(1); s++)
+    {
+        Console.Write( $"{array[x,s]:d3}");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
