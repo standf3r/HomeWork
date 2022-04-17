231dd95085fde229a767a04f1bdaad8770ab5a3d
standfer <vertigl@yandex.ru>
2022-04-17 17:48:13 +0800

    Задача 64. Семинар 10

---
+++ Homework_8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N в обратном порядке.
+
+
+Console.Write("Введите число с которого будет идти обратный: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int NaturalNumber(int num)
+{
+    if (num != 0)
+    {
+        Console.Write($"{num:d} ");
+        NaturalNumber(num-1);
+    }
+    return num;
+}
+NaturalNumber(a);
