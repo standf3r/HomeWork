e2f9ae040af028f7bde13165101030fd0715dcad
standfer <vertigl@yandex.ru>
2022-04-17 16:03:13 +0800

    Задача 43. Семинар 6

---
+++ Homework_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 43. Напишите программу, которая которая найдет точку пересечения двух прямых.
+
+
+Console.Write("Введите b1: ");
+double b1 = int.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = int.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = int.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = int.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+
+Console.WriteLine($"Точка пересечения двух прямых X = {x:0.000} , Y = {y:0.000}\n");
